8ae1fba35ae2a4d0fd549edf1da67c2cd012c149
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-25 05:14:56 -0300

    removidos types

--- src/utils/SecurityAlarm.ts
+++ src/utils/SecurityAlarm.ts
@@ -1,5 +1,5 @@
-import type { ObjectAlarm } from '../types/ObjetctAlarm.js';
-import type { SecurityAlarm } from '../types/SecurityAlarm.js';
+import { ObjectAlarm } from '../types/ObjetctAlarm.js';
+import { SecurityAlarm } from '../types/SecurityAlarm.js';
 
 const securityAlarm: ObjectAlarm = {
     getAudio(): SecurityAlarm {
